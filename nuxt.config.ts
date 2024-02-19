cdd001aa61164494dfd59db20eb1d90d744bb22b
Abhishek Anand <abhishek.anand@hexafoldtech.com>
2024-02-19 12:13:27 +0530

    setup with vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,12 @@
-import { Bar } from 'vue-chartjs';
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-  build: {
-    transpile: ['vuetify'],
-  },
-  modules: [
-    (_options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', (config) => {
-        // @ts-expect-error
-        config.plugins.push(vuetify({ autoImport: true }))
-      })
-    },
-    //...
-  ],
-  vite: {
-    vue: {
-      template: {
-        transformAssetUrls,
-      },
+  app: {
+    head: {
+      title: "Nova-Gas",
     },
   },
-  css:["@/assets/styles/scss/main.scss","@fortawesome/fontawesome-svg-core/styles.css"],
-  plugins: ['@/plugins/chart.ts'],
+  devtools: { enabled: true },
+  css: ["@/assets/scss/index.scss"],
+  modules: ["@pinia/nuxt",'@element-plus/nuxt'],
+
 })
--- plugins/chart.ts
+++
@@ -1,4 +0,0 @@
-import { Chart, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, PieController, ArcElement, DoughnutController } from 'chart.js'
-export default defineNuxtPlugin(() => {
-    Chart.register(CategoryScale, LinearScale, BarElement, ArcElement,DoughnutController, PieController, Title, Tooltip, Legend)
-})
